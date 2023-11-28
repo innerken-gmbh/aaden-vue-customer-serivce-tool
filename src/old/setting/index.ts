18055a4c270c744e6bc2405bdd651609e105a2d8
Haodong Ju <25146689+juhaodong@users.noreply.github.com>
2023-11-28 12:37:54 +0100

    改动一些默认配置

--- src/old/setting/index.ts
+++ src/old/setting/index.ts
@@ -1,27 +1,20 @@
-import {
-  AppConfigState,
-  DeviceType,
-  LayoutMode,
-  PageAnim,
-  SideTheme,
-  ThemeMode,
-} from '@/store/types'
+import {AppConfigState, DeviceType, LayoutMode, PageAnim, SideTheme, ThemeMode,} from '@/store/types'
 
-export const projectName = 'Aaden POS 客服平台'
+export const projectName = 'Aaden 客服平台'
 
 export default {
-  theme: ThemeMode.LIGHT,
-  sideTheme: SideTheme.WHITE,
+  theme: ThemeMode.DARK,
+  sideTheme: SideTheme.DARK,
   themeColor: '#409eff',
-  layoutMode: LayoutMode.LTR,
+  layoutMode: LayoutMode.TTB,
   sideWidth: 210,
   deviceType: DeviceType.PC,
   pageAnim: PageAnim.OPACITY,
   isFixedNavBar: true,
   isCollapse: false,
   actionBar: {
-    isShowSearch: true,
-    isShowMessage: true,
+    isShowSearch: false,
+    isShowMessage: false,
     isShowRefresh: true,
     isShowFullScreen: true,
   },
