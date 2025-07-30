6850e32a32a58edfd97c79912ec343b23b60ad28
噢嚯 <88487061+513355236@users.noreply.github.com>
2025-07-30 17:38:18 +0800

    加入通过key查DeviceId

--- src/views/store-management/reservation-key.vue
+++ src/views/store-management/reservation-key.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main-container pa-4">
+  <div class="main-container d-flex pa-4">
     <v-card
       class="pa-4"
       max-width="600px"
@@ -40,6 +40,46 @@
         </div>
       </div>
     </v-card>
+    <v-card
+      class="pa-4 ml-10"
+      max-width="600px"
+    >
+      <div class="d-flex align-center justify-center text-h5 mb-4">
+        Welcome To Aaden Reservation
+      </div>
+      <div class="d-flex flex-column">
+        <div class="d-flex align-center justify-center">
+          <div>
+            Key
+          </div>
+          <v-spacer />
+          <v-text-field
+            v-model="defaultKeywords"
+            width="500px"
+          />
+        </div>
+        <div class="d-flex align-center justify-center">
+          <div>
+            DeviceId
+          </div>
+          <v-spacer />
+          <v-text-field
+            v-model="searchDeviceId"
+            width="500px"
+          />
+        </div>
+        <div class="d-flex align-center justify-center">
+          <v-btn
+            color="blue lighten-2"
+            variant="outlined"
+            width="100%"
+            @click="checkDeviceId"
+          >
+            查看Key
+          </v-btn>
+        </div>
+      </div>
+    </v-card>
   </div>
 </template>
 
@@ -49,10 +89,27 @@ import {onMounted, ref} from "vue";
 
 const deviceId = ref('')
 const keywords = ref('')
+const defaultKeywords = ref('')
+const searchDeviceId = ref('')
 
 function checkKey () {
   createKey()
 }
+
+function checkDeviceId() {
+  let originalStr = '';
+  for (let i = 0; i < defaultKeywords.value.length; i++) {
+    const char = defaultKeywords.value[i];
+    const charCode = char.charCodeAt(0) - 65; // 0-25
+    // 反向计算数字值
+    const digit = (charCode - i * 3 + 26) % 26;
+    originalStr += digit.toString();
+  }
+
+  // 去除前导0并转为数字
+  searchDeviceId.value = parseInt(originalStr);
+}
+
 
 async function createKey() {
   // 将输入转换为字符串
